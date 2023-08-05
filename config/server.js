a2c033659c8c8f2e87f0fb0a144a1d035a8182e7
qk7p <1nssine@gmail.com>
2023-08-05 15:00:43 +0300

    fix url

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '127.0.0.1'),
   port: env.int('PORT', 1333),
-  url: env("PUBLIC_URL", "http://citrolux.qk7p.ru/cms"),
+  url: env("PUBLIC_URL", "https://citrolux.qk7p.ru/cms"),
   app: {
     keys: env.array('APP_KEYS'),
   },
